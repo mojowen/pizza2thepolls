6074680b18282c2cf58244efe69531993f6283c8
Noah Manger <noah.manger@zapier.com>
2018-09-08 16:58:58 -0700

    Remove geolocate

--- js/main.js
+++ js/main.js
@@ -209,24 +209,6 @@ function fillInAddress() {
   }
 }
 
-// Bias the autocomplete object to the user's geographical location,
-// as supplied by the browser's 'navigator.geolocation' object.
-function geolocate() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      var geolocation = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-      };
-      var circle = new google.maps.Circle({
-        center: geolocation,
-        radius: position.coords.accuracy
-      });
-      autocomplete.setBounds(circle.getBounds());
-    });
-  }
-}
-
 function handleSubmit() {
   /* TODO:
   - Check for required values
